2ff20eb19a90e4fcf2a7ba2afd79401781ad2398
Mikkel85 <sgt.mich@gmail.com>
2018-10-22 13:59:23 +0200

    Working 1 pice pussel finder
    cuts off the padding that crosscorelation ads and can place and show where the piece fits

--- one_peice_cutteing_and_fitting.m
+++ one_peice_cutteing_and_fitting.m
@@ -3,17 +3,23 @@ clear; close all; clc
 
 %% Subtracting the Background
 figure
+%Load in white back
 Back = imread ('hvid_baggrund.jpg');
+% Convert to Double data type
 Back = im2double(Back);
 Front = imread ('brik 1 reel.jpg');
 Front = im2double(Front);
 
+%subtract the forground from the backround
 diffImage = Front - Back;
-mask = abs(diffImage) > 0.35;
+% finding the highest values over a certain threshold
+Threshold = 0.35;
+mask = abs(diffImage) > Threshold;
+% Convert to Double data type
 mask = im2double(mask);
-
+% detmining if any element of the mask array is non zero
 mask = any (mask,3);
-
+%plotting the results
 subplot (1,2,1);
 mask = all(mask,3);
 imshow (mask);
@@ -21,33 +27,60 @@ imshow (mask);
 % use the mask to mark the changed area in the picture.
 subplot (1,2,2);
 box = regionprops(mask,'Area', 'BoundingBox'); 
-% Boundingbox [left, top, width, height]
-
+% cutting the image to fit the size of the mask from values in boundingbox
 rect = box(1).BoundingBox;
 % Boundingbox [left, top, width, height]
-rect = [rect(1)+45 rect(2)+45 rect(3)-90 rect(4)-90];
 
+% creating square from values in the image and cutting away the white parts
+rect = [rect(1)+45 rect(2)+45 rect(3)-90 rect(4)-90];
+%rect = [rect(1) rect(2) rect(3) rect(4)];
+% cropping the front image to fit the dimensions given in the rect varible
 piece1 = imcrop (Front,rect);
 imshow (piece1);
 
 %% 2D cross corlation
 
-Original = imread('med hvid baggrund.jpg'); 
+% loading in the image of the puzzle
+Original = imread('med hvid baggrund.jpg');
+
+% converting to grayscale
 ref = rgb2gray(Original); 
+
+% converting to double data type
 ref = im2double(ref);
 
+% finding the nigative of the image 
 nref = ref-mean(mean(ref));
 
+% making the next section easily reuseable
 brik = piece1;
 brik = rgb2gray(brik);
 brik = im2double(brik);
 
+% taking the cross correlation of the piece found earlier with the negative
+% of the full puzzle
 crr = normxcorr2(brik,nref);
 
+% finding the width and height of the piece
+[h,w] = size(brik);
+
+% removing the padding that the cross corelation ads
+crr=crr(h:end-h,w:end-w+1);
+
+% finding the maximum value of the cross correlation
+% ssr is the pixel index for the highest values
+% snd is the highest values, value
 [ssr,snd] = max(crr(:));
-siz = size(Original);
-[I,J] = ind2sub(siz,snd);
-%I = 2048-I;
+siz = size(crr);
+
+% converting the pixel index to a subscript that can be used in a
+% coordinate system
+[Y,X] = ind2sub(siz,snd);
+
+% subtraction of the offset made when cross correlating
+Y_offset = Y-size(brik,1);
+X_offset = X-size(brik,2);
+
 % plot of the cross correlation
 figure
 subplot(1,2,1)
@@ -61,13 +94,13 @@ subplot(1,2,2)
 surf(crr)
 shading flat
 
-kimg(I:-1:I-size(brik,1)+1,J:-1:J-size(brik,2)+1) = rot90(brik,2);
 %% Show Placement
 figure
+% showing the image with scaled colors
 imagesc(Original)
 hold on 
-[w,h] = size(brik);
-rectangle ('position',[I J w h],'EdgeColor','r')
+% drawing a rectangle where the piece is suppossed to go
+rectangle ('position',[X+1 Y+1 w h],'EdgeColor','r')
 axis image off
 colormap gray
 title('Show Piece placement')
